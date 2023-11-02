4003a8db4702000387ca5e55d17f49042e03987e
Dmitrii Shpilberg <dmitriishpilberg@gmail.com>
2023-11-02 19:05:42 +0300

    2nd binary

---
+++ binary_search/2-badVersion.py
@@ -0,0 +1,18 @@
+#isBadVersion def is not defined here.this is an example to be used in the future
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+
+        left, right = 0, n
+        while left < right:
+            mid = (left + right) // 2
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+
+        # Post-processing:
+        # End Condition: left == right
+        if isBadVersion(left):
+            return left
+        return -1
